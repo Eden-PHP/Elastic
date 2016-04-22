e3853ec628f872caf868eefb1ce4d45716360b40
Charles Zamora <charles.andacc@gmail.com>
2016-04-22 19:12:22 +0800

    re-write to make it more simple

--- src/Index.php
+++ src/Index.php
@@ -20,141 +20,36 @@ namespace Eden\Elastic;
 class Index extends Resource
 {
     /**
-     * Document name required error.
-     *
-     * @const string
-     */
-    const DOCUMENT_NAME_REQUIRED = 'Document name is required.';
-
-    /**
-     * Document type required error.
-     *
-     * @const string
-     */
-    const DOCUMENT_TYPE_REQUIRED = 'Document type is required.';
-
-    /**
-     * Document data required error.
-     *
-     * @const string
-     */
-    const DOCUMENT_DATA_REQUIRED = 'Document data is required.';
-
-    /**
      * Unable to connect error.
      *
      * @const string
      */
-    const UNABLE_TO_CONNECT = 'Unable to connect to host: %s';
+    const UNABLE_TO_CONNECT = 'Unable to connect to: %s';
 
     /**
-     * Index id required error.
-     *
-     * @const string
-     */
-    const INDEX_ID_REQUIRED = 'Document index id is required.';
-
-    /**
-     * Connect to elastic api.
+     * Connects to elastic api.
      *
      * @return  Eden\Elastic\Index
      */
     public function connect()
     {
-        // initialize resource
-        $this->resource = \Eden\Curl\Index::i();
-
+        // let's try to connect
         try {
-            // let's test the connection
-            $response = $this->resource
-            // set the resource url
-            ->setUrl($this->url) 
-            // get json response
-            ->getJsonResponse();
+            // test request
+            $response = $this->request(self::GET, '');
         } catch(\Exception $e) {
-            // throw up an exception
-            return Exception::i($e->getMessage())->trigger();
+            // throw an exception
+            return Exception::i(self::UNABLE_TO_CONNECT)->trigger();
         }
 
-        // do we have a response?
-        if(empty($response)) {
-            // throw up an exception
-            return Exception::i(sprintf(self::UNABLE_TO_CONNECT, $this->url))->trigger();
-        }
+        // set connection information
+        $this->elastic      = $response;
+        // set connected flag
+        $this->connected    = true;
 
-        // set the connection information
-        $this->elastic = $response;
+        echo '<pre>';
+        print_r($this);
 
         return $this;
-    }
-
-    /**
-     * Index a single record.
-     *
-     * @param   string
-     * @param   string
-     * @param   array
-     * @param   array
-     * @return  array
-     */
-    public function index($document, $type, $data, $options = array())
-    {
-        // build up the url
-        $url = $this->url . '/' . $document . '/' . $type;
-
-        // if document is not set
-        if(!isset($document)) {
-            return Exception::i(self::DOCUMENT_NAME_REQUIRED)->trigger();
-        }
-
-        // if type is not set
-        if(!isset($type)) {
-            return Exception::i(self::DOCUMENT_TYPE_REQUIRED)->trigger();
-        }
-
-        // does data empty?
-        if(empty($data)) {
-            return Exception::i(self::DOCUMENT_DATA_REQUIRED)->trigger();
-        }
-
-        // does index id set?
-        if(!isset($data['_id'])) {
-            return Exception::i(self::INDEX_ID_REQUIRED)->trigger();
-        }
-
-        // set that id unto our url
-        $url = $url . '/' . $data['_id'];
-
-        // unset the id from the data
-        unset($data['_id']);
-        
-        // do we have options?
-        if(!empty($options)) {
-            // set the options
-            $this->setOptions($options);
-        }
-
-        // set the document
-        $this->document = $document;
-
-        // set the type
-        $this->type = $type;
-
-        // set the url
-        $this->url = $url;
-
-        // let's send this up
-        return $this->request('PUT', $data, array(), array());
-    }
-
-    /**
-     * Debug purposes.
-     *
-     * @param   string
-     * @return  Eden\Core\Inspect
-     */
-    public function debug($message)
-    {
-        return \Eden\Core\Inspect::i()->inspect($message);
     }
 }
--- src/Resource.php
+++ src/Resource.php
@@ -20,124 +20,93 @@ namespace Eden\Elastic;
 abstract class Resource extends Base
 {
     /**
-     * Failed request error.
+     * GET Method.
      *
      * @const string
      */
-    const FAILED_REQUEST = 'An error occured while sending request.';
+    const GET = 'GET';
+
+    /**
+     * POST Method.
+     *
+     * @const string
+     */
+    const POST = 'POST';
+
+    /**
+     * PUT Method.
+     *
+     * @const string
+     */
+    const PUT = 'PUT';
+
+    /**
+     * DELETE Method.
+     *
+     * @const string
+     */
+    const DELETE = 'DELETE';
+
+    /**
+     * Error sending request.
+     *
+     * @const string
+     */
+    const REQUEST_ERROR = 'An error occured while sending request.';
 
     /**
      * Elastic api host.
      *
      * @var string
      */
-    protected $host = 'localhost';
+    protected $host = 'http://localhost:9200';
 
     /**
-     * Elastic api port.
-     *
-     * @var int
-     */
-    protected $port = 9200;
-
-    /**
-     * Elastic api http protocol.
-     *
-     * @var bool
-     */
-    protected $secure = false;
-
-    /**
-     * Current connection url.
-     *
-     * @param string
-     */
-    protected $url = null;
-
-    /**
-     * Sets the post data.
-     *
-     * @var array
-     */
-    protected $data = array();
-
-    /**
-     * Default document that
-     * we are going to use.
+     * Elastic api index.
      *
      * @var string
      */
-    protected $document = null;
+    protected $index = null;
 
     /**
-     * Document type that we
-     * are going to use.
-     *
-     * @var string
-     */
-    protected $type = null;
-
-    /**
-     * Set query options.
-     *
-     * @var array
-     */
-    protected $options = array();
-
-    /**
-     * Optional request headers.
+     * Custom headers.
      *
      * @var array
      */
     protected $headers = array();
 
     /**
-     * Connection resource.
+     * Custom request query.
      *
-     * @var Eden\Curl\Index
+     * @var array
      */
-    protected $resource = null;
+    protected $query = array();
 
     /**
-     * Initialize elastic resource.
+     * Builds out connection information.
      *
      * @param   string
-     * @param   int
-     * @param   bool
-     * @param   array
-     * @return  Eden\Elastic\Index
+     * @param   string
+     * @return  Eden\Elastic\Resource
      */
-    public function __construct(
-        $host = 'localhost',
-        $port = 9200,
-        $secure = false,
-        $options = array())
+    public function __construct($host = 'http://localhost:9200', $index = null)
     {
-        // set the host
-        $this->host    = $host;
-        // set the port
-        $this->port    = $port;
-        // set the secure
-        $this->secure  = $secure;
-        // set the headers
-        $this->headers = array();
+        // Argument test
+        Argument::i()
+            ->test(1, 'string')
+            ->test(2, 'string', 'null');
 
-        // what's our protocol?
-        $protocol = 'http';
+        // set elastic api host
+        $this->host  = $host;
+        // set elastic api index
+        $this->index = $index;
 
-        // if we're on secure
-        if($this->secure) {
-            $protocol = 'https';
-        }
-
-        // build out the connection url
-        $this->url = $protocol . '://' . $this->host . ':' . $this->port;
-
-        return $this;
+        // initialize our resource
+        $this->resource = \Eden\Curl\Index::i();
     }
 
     /**
-     * Abstract connect function signature.
+     * Abstract connect method signature.
      *
      * @return  Eden\Elastic\Resource
      */
@@ -147,69 +116,55 @@ abstract class Resource extends Base
      * Set elastic api host.
      *
      * @param   string
-     * @return  Eden\Elastic\Index
+     * @return  Eden\Elastic\Resource
      */
     public function setHost($host)
     {
-        // set host
+        // Argument test
+        Argument::i()->test(1, 'string');
+    
+        // set elastic api host
         $this->host = $host;
 
         return $this;
     }
 
     /**
-     * Set elastic api port.
+     * Set elastic api index.
      *
-     * @param   int
-     * @return  Eden\Elastic\Index
+     * @param   string
+     * @return  Eden\Elastic\Resource
      */
-    public function setPort($port)
+    public function setIndex($index)
     {
-        // set port
-        $this->port = $port;
+        // Argument test
+        Argument::i()->test(1, 'string');
+
+        // set elastic api index
+        $this->index = $index;
 
         return $this;
     }
 
-    /**
-     * Set elastic secure flag.
-     *
-     * @param   bool
-     * @return  Eden\Elastic\Index
-     */
-    public function setSecure($secure)
-    {
-        // set secure
-        $this->secure = $secure;
-
-        return $this;
-    }
-
-    /**
-     * Set request data.
+    /** 
+     * Set custom list of headers.
      *
      * @param   array
-     * @return  Eden\Elastic\Base
+     * @return  Eden\Elastic\Resource
      */
-    public function setData($data = array())
+    public function setHeaders($headers = array())
     {
-        // set data
-        $this->data = $data;
+        // if headers set is empty
+        if(empty($headers)) {
+            // set empty headers
+            $this->headers = array();
 
-        return $this;
-    }
+            return $this;
+        }
 
-    /**
-     * Set optional headers.
-     *
-     * @param   array
-     * @return  Eden\Elastic\Base
-     */
-    public function setHeaders($headers)
-    {
-        // set headers
+        // iterate on each headers
         foreach($headers as $key => $value) {
-            // set each headers
+            // set header
             $this->headers[$key] = $value;
         }
 
@@ -217,159 +172,181 @@ abstract class Resource extends Base
     }
 
     /**
-     * Add optional header.
+     * Add custom header.
      *
      * @param   string
-     * @param   *mixed
-     * @return  Eden\Elastic\Base
+     * @param   *midex
+     * @return  Eden\Elastic\Resource
      */
     public function addHeader($key, $value)
     {
-        // set optional header
-        $this->headers[$key] = $value;
+        // set header
+        $this->header[$key] = $value;
 
         return $this;
     }
 
     /**
-     * Set the document that we
-     * are going to use.
-     *
-     * @param   string
-     * @return  Eden\Elastic\Base
-     */
-    public function setDocument($document)
-    {
-        // set default document
-        $this->document = $document;
-
-        return $this;
-    }
-
-    /**
-     * Set the type under a document.
-     *
-     * @param   string
-     * @return  Eden\Elastic\Base
-     */
-    public function setType($type)
-    {
-        // set default document type
-        $this->type = $type;
-
-        return $this;
-    }
-
-    /**
-     * Set request options.
+     * Set custom request queries.
      *
      * @param   array
-     * @return  Eden\Elastic\Base
+     * @return  Eden\Elastic\Resource
      */
-    public function setOptions($options)
+    public function setQuery($query = array())
     {
-        // set options
-        foreach($options as $key => $value) {
-            $this->options[$key] = $value;    
+        // if query is empty
+        if(empty($query)) {
+            // set empty query
+            $this->query = array();
+        }
+
+        // iterate on each query
+        foreach($query as $key => $value) {
+            // set query
+            $this->query[$key] = $value;
         }
 
         return $this;
     }
 
     /**
-     * Add option to the current set
-     * of request options.
+     * Add custom request query.
      *
      * @param   string
      * @param   *mixed
-     * @return  Eden\Elastic\Base
+     * @return  Eden\Elastic\Resource
      */
-    public function addOption($key, $value)
+    public function addQuery($key, $value)
     {
-        // add option
-        $this->options[$key] = $value;
+        // add custom query
+        $this->query[$key] = $value;
 
         return $this;
     }
 
     /**
-     * Basic request.
+     * Sets basic authorization (TODO).
      *
+     * @param   string
+     * @param   string
+     * @return  Eden\Elastic\Resource
+     */
+    public function setBasicAuthorization($username, $password) 
+    {
+        // Argument test
+        Argument::i()
+            ->test(1, 'string')
+            ->test(2, 'string');
+
+        // :TODO
+
+        return $this;
+    }
+
+    /**
+     * Basic request method.
+     *
+     * @param   string
      * @param   string
      * @param   array
      * @param   array
      * @param   array
      * @return  array
      */
-    public function request($method, $data = array(), $query = array(), $headers = array())
+    public function request(
+        $method, 
+        $endpoint, 
+        $data = array(), 
+        $query = array(), 
+        $headers = array())
     {
-        // set the request url
+        // Argument test
+        Argument::i()
+            ->test(1, 'string')
+            ->test(2, 'string')
+            ->test(3, 'array')
+            ->test(4, 'array')
+            ->test(5, 'array');
+
+        // initialize request
         $request = $this->resource;
 
-        // if data is set
-        if(!empty($data)) {
-            // let's set the request data
-            $this->setData($data);
+        // set endpoint
+        $url = $this->host . '/' . $endpoint;
+
+        // set query
+        if(!empty($query)) {
+            // set query
+            $this->setQuery($query);
         }
 
-        // if request headers is set
+        // set headers
         if(!empty($headers)) {
-            // add everything on our headers
+            // set headers
             $this->setHeaders($headers);
         }
 
-        // do we have extra headers?
-        if(!empty($this->headers)) {
-            // set the headers
-            foreach($headers as $key => $value) {
-                // set request headers
-                $request->setHeader($key, $value);
-            }
+        // query set?
+        if(!empty($this->query)) {
+            // set request query
+            $url = $url . '?' . http_build_query($this->query);
         }
 
-        // if query is not empty
-        if(!empty($query)) {
-            // set options
-            $this->setOptions($query);
-        }
-
-        // build out the url
-        $url = $this->url;
-
-        // if options are set
-        if(!empty($this->options)) {
-            // set query options
-            $url = $url . '?' . http_build_query($this->options);
-        }
-
-        // let's set the url
+        // set the url
         $request->setUrl($url);
 
-        // if put request
-        if($method == 'PUT') {
-            // set as put request
-            $request->setCustomRequest('PUT')
-            // set content type
-            ->setHeaders('Content-Type', 'application/json')
-            // set post fields
-            ->setPostFields(json_encode($this->data));
-        } else if($method == 'POST') {
-            // set as post request
-            $request->setPost(true)
-            // set content type
-            ->setHeaders('Content-Type', 'application/json')
-            // set post fields
-            ->setPostFields(json_encode($this->data));
-        } else if($method == 'DELETE') {
-            // set custom request
-            $request->setCustomRequest('DELETE');
-        } else {
-            // set as http get
-            $request->setHttpGet(true);
+        // check the method
+        switch ($method) {
+            // GET method?
+            case self::GET:
+                // set http get
+                $request->setHttpGet(true);
+
+                break;
+            // POST method?
+            case self::POST:
+                // set http post
+                $request->setPost(true)
+                // set post fields
+                ->setPostFields(json_encode($data));
+
+                // set headers
+                $this->addHeader('Content-Type', 'application/json');
+
+                break;
+            // PUT method?
+            case self::PUT:
+                // set custom request
+                $request->setCustomRequest(self::PUT)
+                // set post fields
+                ->setPostFields(json_encode($data));
+
+                // set headers
+                $this->addHeader('Content-Type', 'application/json');
+
+                break;
+            // DELETE method?
+            case self::DELETE:
+                // set custom request
+                $request->setCustomRequest(self::DELETE)
+                // set header
+                ->setHeaders('Content-Type', 'application/json');
+                
+                break;
+            default:
+                // set http get
+                $request->setHttpGet(true);
+
+                break;
+        }
+
+        // let's set the headers
+        foreach($this->headers as $key => $value) {
+            $request->setHeaders($key, $value);
         }
 
         try {
-            // let's follow redirects
+            // trigger request
             $response = $request->setFollowLocation(true)
             // set connection timeout
             ->setConnectTimeout(60)
@@ -380,8 +357,8 @@ abstract class Resource extends Base
             // get the response
             ->getJsonResponse();
         } catch(\Exception $e) {
-            // throw an exception
-            return Exception::i(self::FAILED_REQUEST)->trigger();
+            // throw an error
+            return Exception::i(self::REQUEST_ERROR)->trigger();
         }
 
         // do we have an error?
@@ -396,16 +373,5 @@ abstract class Resource extends Base
         }
 
         return isset($response) || !empty($response) ? $response : array();
-    }
-
-    /**
-     * Get the current resource
-     * meta data.
-     *
-     * @return  array
-     */
-    public function getResource()
-    {
-        return get_object_vars($this);
     }
 }
